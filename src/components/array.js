4b950edcfbcd78b8dceb5fbafe357289b046f2f2
HetalVKinger <hetalvkinger@gmail.com>
2022-06-24 09:29:49 +0530

    CRUD

---
+++ src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+  {
+    id: "1",
+    Name: "Hetal",
+    Age: "28",
+  },
+  {
+    id: "2",
+    Name: "Vishal",
+    Age: "33",
+  },
+  {
+    id: "3",
+    Name: "Kinger",
+    Age: "5",
+  }
+];
+
+export default array;
